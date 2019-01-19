ab505c2fe009eb8e155d4713a86a8fcc409d245d
Asma <esikeseraj@yahoo.com>
2019-01-19 18:40:09 +0330

    Initial commit

---
+++ src/ExistingUserException.java
@@ -0,0 +1,1 @@
+class ExistingUserException extends Exception {    public String message ;    public ExistingUserException(){        super();    }    public ExistingUserException(String message){        this.message = message;    }}
